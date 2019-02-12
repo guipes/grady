149ef59fd85987a1b0da12246c24b3851b966ffd
Willa <willar713@gmail.com>
2019-02-12 16:48:29 -0600

    wrote testing for my code

--- grades.py
+++ grades.py
@@ -9,59 +9,61 @@
 from collections import defaultdict
 
 
-# def group(a, b):
-#     dct[a].append(b)
+def group(dct, a, b):
+    dct[a].append(b)
+    return dct
 
 
-# def dct_keys(dictionary, ky):
-#     if dictionary.get(ky) is None:
-#         dictionary.setdefault(ky, [])
-#     return dictionary
+def dct_keys(dictionary, ky):
+    if dictionary.get(ky) is None:
+        dictionary.setdefault(ky, [])
+    return dictionary
 
 
-# def mean(nums):
-#     return sum(nums) / len(nums)
+def mean(nums):
+    return sum(nums) / len(nums)
 
 
-# with open('grades.txt') as f:
-#     dct = dict()
-#     avg_dct = dict()
-#     for line in f:
-#         entry = line.strip().split(' ')
-#         name = entry[0]
-#         grade = float(entry[1])
-#         dct = dct_keys(dct, name)
-#         group(name, grade)
-#     print(dct)
-#     for person in dct:
-#         avg = mean(dct[person])
-#         avg_dct[person] = avg
-#     lst = list(avg_dct.items())
-#     lst.sort(key=lambda x: x[1], reverse=True)
-#     for x, y in lst:
-#         print(x, y)
+def sort(lst):
+    L = list(lst.items())
+    return sorted(L, key=lambda t: t[1])[::-1]
 
+
+with open('grades.txt') as f:
+    dct = dict()
+    avg_dct = dict()
+    for line in f:
+        entry = line.strip().split(' ')
+        name = entry[0]
+        grade = float(entry[1])
+        dct = dct_keys(dct, name)
+        dct2 = group(dct, name, grade)
+    for person in dct2:
+        avg = mean(dct2[person])
+        avg_dct[person] = avg
+    L = sort(avg_dct)
+    print(L)
 
 # --------------- CHYLD'S CODE ----------------
 
 
-def get_grades(fn):
-    grades = defaultdict(list)
-    with open(fn) as f:
-        for line in f:
-            name, grade = line.strip().split(' ')
-            grade = float(grade)
-            grades[name].append(grade)
-    return grades
+# def get_grades(fn):
+#     grades = defaultdict(list)
+#     with open(fn) as f:
+#         for line in f:
+#             name, grade = line.strip().split(' ')
+#             grade = float(grade)
+#             grades[name].append(grade)
+#     return grades
 
 
-def get_averages(grades):
-    avgs = []
-    for name, scores in grades.items():
-        avg = sum(scores) / len(scores)
-        avgs.append((name, avg))
-    return avgs
+# def get_averages(grades):
+#     avgs = []
+#     for name, scores in grades.items():
+#         avg = sum(scores) / len(scores)
+#         avgs.append((name, avg))
+#     return avgs
 
 
-def rank_scores(avgs):
-    return sorted(avgs, key=lambda t: t[1])[::-1]
+# def rank_scores(avgs):
+#     return sorted(avgs, key=lambda t: t[1])[::-1]
--- tests/test_grades.py
+++ tests/test_grades.py
@@ -1,22 +1,55 @@
+from grades import group, dct_keys, mean, sort
+
+
+def test_mean():
+    m = mean([1, 2, 3, 4, 5])
+    assert m == 3
+
+
+def test_dct_keys():
+    d = {'sally': []}
+    key1 = 'sam'
+    key2 = 'sally'
+    key3 = 'sam'
+    key4 = 'bob'
+    dct_keys(d, key1)
+    dct_keys(d, key2)
+    dct_keys(d, key3)
+    dct_keys(d, key4)
+    assert d == {'sally': [], 'sam': [], 'bob': []}
+
+
+def test_group():
+    a = 'bob'
+    b = 78
+    d = {'sally': [], 'sam': [], 'bob': []}
+    new_dct = group(d, a, b)
+    assert new_dct == {'sally': [], 'sam': [], 'bob': [78]}
+
+
+def test_sort():
+    us = {'sally': 27, 'sam': 98, 'bob': 78}
+    s = sort(us)
+    assert s == [('sam', 98), ('bob', 78), ('sally', 27)]
 # --------------- CHYLD'S CODE ----------------
-from grades import get_grades, get_averages, rank_scores
+# from grades import get_grades, get_averages, rank_scores
 
 
-def test_get_grades():
-    grades = get_grades('grades.txt')
-    print(grades)
-    d = {'bob': [78.0, 88.0, 90.0], 'sara': [98.0], 'sue': [72.0, 95.0]}
-    assert grades == d
+# def test_get_grades():
+#     grades = get_grades('grades.txt')
+#     print(grades)
+#     d = {'bob': [78.0, 88.0, 90.0], 'sara': [98.0], 'sue': [72.0, 95.0]}
+#     assert grades == d
 
 
-def test_get_averages():
-    grades = get_grades('grades.txt')
-    avgs = get_averages(grades)
-    assert avgs == [('bob', 85.33333333333333), ('sara', 98.0), ('sue', 83.5)]
+# def test_get_averages():
+#     grades = get_grades('grades.txt')
+#     avgs = get_averages(grades)
+#     assert avgs == [('bob', 85.33333333333333), ('sara', 98.0), ('sue', 83.5)]
 
 
-def test_rank_scores():
-    grades = get_grades('grades.txt')
-    avgs = get_averages(grades)
-    rank = rank_scores(avgs)
-    assert rank == [('sara', 98.0), ('bob', 85.33333333333333), ('sue', 83.5)]
+# def test_rank_scores():
+#     grades = get_grades('grades.txt')
+#     avgs = get_averages(grades)
+#     rank = rank_scores(avgs)
+#     assert rank == [('sara', 98.0), ('bob', 85.33333333333333), ('sue', 83.5)]
